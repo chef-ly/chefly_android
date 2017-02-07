dcca2fad8b5de8ca7a7c029a9ff1d4268cfeb643
AthVer <athina.verroiopoulou>
2017-02-07 00:36:48 -0600

    Network Connection

--- app/src/main/java/com/se491/chef_ly/activity/MainActivity.java
+++ app/src/main/java/com/se491/chef_ly/activity/MainActivity.java
@@ -2,24 +2,43 @@ package com.se491.chef_ly.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 
 import com.se491.chef_ly.R;
+import com.se491.chef_ly.http.MyService;
+import com.se491.chef_ly.utils.NetworkHelper;
 
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity implements View.OnClickListener {
 
     private EditText username;
     private EditText password;
     private Button signInBtn;
     private TextView continueAsGuest;
     private TextView signUp;
+    private boolean netExist;
+    private static final String urlString = "https://pure-fortress-13559.herokuapp.com/recipe/";
+    //TextView output;
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) { //receive the data , message that i am looking for
+            String message =
+                    intent.getStringExtra(MyService.MY_SERVICE_PAYLOAD);
+            continueAsGuest.append(message + "\n");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +50,20 @@ public class MainActivity extends Activity implements View.OnClickListener{
         // test call to get Recipe list
         //new GetRecipeList(this);
         //new GetRecipe(this, 2);
-        Toast.makeText(getApplicationContext(),"Welcome To Chef.ly",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Welcome To Chef.ly", Toast.LENGTH_SHORT).show();
+            //listen to the message
+        LocalBroadcastManager.getInstance(getApplicationContext())
+                .registerReceiver(mBroadcastReceiver,
+                        new IntentFilter(MyService.MY_SERVICE_MESSAGE));
+
+        netExist = NetworkHelper.hasNetworkAccess(this);
+        continueAsGuest.append("Network ok: " + netExist);
     }
 
-    private void setupViews(){
+    private void setupViews() {
 
-        username = (EditText)findViewById(R.id.username);
-        password = (EditText)findViewById(R.id.password);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
         signInBtn = (Button) findViewById(R.id.signInBtn);
         signInBtn.setOnClickListener(this);
         continueAsGuest = (TextView) findViewById(R.id.continueAsGuest);
@@ -49,23 +75,30 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
+        //register to listen the data
+        Intent intent = new Intent(this, MyService.class);
+        intent.setData(Uri.parse(urlString));
+        startService(intent);
+        startService(intent);
+        startService(intent);
+
+        switch (v.getId()) {
 
             case R.id.signInBtn:
 
                 Editable user = username.getText();
                 Editable pword = password.getText();
-                if(user.length() == 0){
+                if (user.length() == 0) {
                     Toast.makeText(this, "Username cannot be blank", Toast.LENGTH_SHORT).show();
-                }else if(pword.length() == 0){
+                } else if (pword.length() == 0) {
                     Toast.makeText(this, "Password cannot be blank", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     boolean isAllowed = authenticate();
-                    if(isAllowed){
+                    if (isAllowed) {
                         Intent recipeListIntent = new Intent(MainActivity.this, RecipeListActivity.class);
-                        recipeListIntent.putExtra("name",user.toString());
+                        recipeListIntent.putExtra("name", user.toString());
                         startActivity(recipeListIntent);
-                    }else{
+                    } else {
                         Toast.makeText(this, "Invalid Username or password", Toast.LENGTH_SHORT).show();
                     }
                 }
@@ -73,7 +106,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 break;
             case R.id.continueAsGuest:
                 Intent recipeListIntent = new Intent(MainActivity.this, RecipeListActivity.class);
-                recipeListIntent.putExtra("name","guest");
+                recipeListIntent.putExtra("name", "guest");
                 startActivity(recipeListIntent);
                 break;
             case R.id.signUp:
@@ -84,10 +117,21 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
         }
     }
-    private boolean authenticate(){
+
+    private boolean authenticate() {
         //TODO contact server and authenticate user
         return true;
     }
+    @Override
+    protected void onDestroy() { //unregister to listen the data
+        super.onDestroy();
+
+        LocalBroadcastManager.getInstance(getApplicationContext())
+                .unregisterReceiver(mBroadcastReceiver);
+    }
+
+
+
 }
 
 
---
+++ app/src/main/java/com/se491/chef_ly/http/MyService.java
@@ -0,0 +1,52 @@
+package com.se491.chef_ly.http;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.support.v4.content.LocalBroadcastManager;
+
+//manage request with the use of IntentService
+public class MyService extends IntentService {
+    public static final String TAG = "MyService";
+    public static final String MY_SERVICE_MESSAGE = "myServiceMessage";
+    public static final String MY_SERVICE_PAYLOAD = "myServicePayload";
+
+
+
+    public MyService() {
+        super("MyService");
+    }
+    //called automaticly when server starts, receives data as an argument
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Uri uri = intent.getData();
+        Log.i(TAG, "onHandleIntent: " + uri.toString());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Intent messageIntent = new Intent(MY_SERVICE_MESSAGE);
+        messageIntent.putExtra(MY_SERVICE_PAYLOAD, "Service all done!"); //pass data back, set key value and message
+        //package the data
+        LocalBroadcastManager manager =
+                LocalBroadcastManager.getInstance(getApplicationContext());
+        manager.sendBroadcast(messageIntent); //send / broadcast the message
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+}
