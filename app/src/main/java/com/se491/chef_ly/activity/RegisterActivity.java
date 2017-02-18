7360225cf364e48d179461aaed230f3ce4266fee
David Chang <davidshchang@gmail.com>
2017-02-18 12:30:37 -0600

    add layout and activity for register

---
+++ app/src/main/java/com/se491/chef_ly/activity/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.se491.chef_ly.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.se491.chef_ly.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
