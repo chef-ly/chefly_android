da8543e830a07ef96f30474f917e3b3cd5d04a4a
AthVerr <athina.verroiopoulou@gmail.com>
2017-03-02 22:08:05 -0600

    coverage

--- app/src/androidTest/java/com/se491/chef_ly/TestUI.java
+++ app/src/androidTest/java/com/se491/chef_ly/TestUI.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import com.se491.chef_ly.R;
 import com.se491.chef_ly.activity.MainActivity;
 
 import org.hamcrest.Description;
@@ -22,6 +23,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -29,13 +31,13 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class GetCookingTest {
+public class TestUI {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void getCookingTest() {
+    public void testUI() {
         ViewInteraction appCompatTextView = onView(
                 allOf(withId(R.id.continueAsGuest), withText("Continue As Guest"), isDisplayed()));
         appCompatTextView.perform(click());
@@ -48,9 +50,28 @@ public class GetCookingTest {
                         isDisplayed()));
         linearLayout.perform(click());
 
+        ViewInteraction checkBox = onView(
+                allOf(withText("linquine pasta - 4.0 ounce"),
+                        withParent(withId(R.id.ingredientGroup))));
+        checkBox.perform(scrollTo(), click());
+
+        ViewInteraction checkBox2 = onView(
+                allOf(withText("boneless, skinless chicken breasts - 2.0 breasts"),
+                        withParent(withId(R.id.ingredientGroup))));
+        checkBox2.perform(scrollTo(), click());
+
+        ViewInteraction checkBox3 = onView(
+                allOf(withText("Cajun Seasoning - 2.0 tespoons"),
+                        withParent(withId(R.id.ingredientGroup))));
+        checkBox3.perform(scrollTo(), click());
+
         ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.getCookingBtn), withText("Let's get cooking!")));
+                allOf(withId(R.id.addToListBtn), withText("Add Selected to Grocery List")));
         appCompatButton.perform(scrollTo(), click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.getCookingBtn), withText("Let's get cooking!")));
+        appCompatButton2.perform(scrollTo(), click());
 
         ViewInteraction button = onView(
                 allOf(withId(R.id.next), withText("Next"),
@@ -86,6 +107,32 @@ public class GetCookingTest {
                                 withParent(withId(android.R.id.content)))),
                         isDisplayed()));
         button5.perform(click());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatCheckedTextView = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Shopping List"), isDisplayed()));
+        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction linearLayout2 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.list),
+                                withParent(withId(R.id.activity_shopping_list))),
+                        0),
+                        isDisplayed()));
+        linearLayout2.perform(click());
+
+        ViewInteraction button6 = onView(
+                allOf(withId(R.id.clearPurchasedBtn), withText("Remove Purchased"), isDisplayed()));
+        button6.perform(click());
+
+        ViewInteraction button7 = onView(
+                allOf(withId(R.id.finishedBtn), withText("Finished"), isDisplayed()));
+        button7.perform(click());
 
     }
 
--- app/src/androidTest/java/com/se491/chef_ly/test/login.java
+++ app/src/androidTest/java/com/se491/chef_ly/test/login.java
@@ -1,43 +1,43 @@
-package com.se491.chef_ly.test;
-
-import com.se491.chef_ly.activity.MainActivity;
-import com.robotium.solo.*;
-import android.test.ActivityInstrumentationTestCase2;
-
-
-public class login extends ActivityInstrumentationTestCase2<MainActivity> {
-  	private Solo solo;
-  	
-  	public login() {
-		super(MainActivity.class);
-  	}
-
-  	public void setUp() throws Exception {
-        super.setUp();
-		solo = new Solo(getInstrumentation());
-		getActivity();
-  	}
-  
-   	@Override
-   	public void tearDown() throws Exception {
-        solo.finishOpenedActivities();
-        super.tearDown();
-  	}
-  
-	public void testRun() {
-        //Wait for activity: 'com.se491.chef_ly.activity.MainActivity'
-		solo.waitForActivity(com.se491.chef_ly.activity.MainActivity.class, 2000);
-        //Enter the text: 'a'
-		solo.clearEditText((android.widget.EditText) solo.getView(com.se491.chef_ly.R.id.username));
-		solo.enterText((android.widget.EditText) solo.getView(com.se491.chef_ly.R.id.username), "a");
-        //Click on Empty Text View
-		solo.clickOnView(solo.getView(com.se491.chef_ly.R.id.password));
-        //Enter the text: 'a'
-		solo.clearEditText((android.widget.EditText) solo.getView(com.se491.chef_ly.R.id.password));
-		solo.enterText((android.widget.EditText) solo.getView(com.se491.chef_ly.R.id.password), "a");
-        //Click on Sign In
-		solo.clickOnView(solo.getView(com.se491.chef_ly.R.id.signInBtn));
-        //Wait for activity: 'com.se491.chef_ly.activity.RecipeListActivity'
-		assertTrue("com.se491.chef_ly.activity.RecipeListActivity is not found!", solo.waitForActivity(com.se491.chef_ly.activity.RecipeListActivity.class));
-	}
-}
+//package com.se491.chef_ly.test;
+//
+//import com.se491.chef_ly.activity.MainActivity;
+//import com.robotium.solo.*;
+//import android.test.ActivityInstrumentationTestCase2;
+//
+//
+//public class login extends ActivityInstrumentationTestCase2<MainActivity> {
+//  	private Solo solo;
+//
+//  	public login() {
+//		super(MainActivity.class);
+//  	}
+//
+//  	public void setUp() throws Exception {
+//        super.setUp();
+//		solo = new Solo(getInstrumentation());
+//		getActivity();
+//  	}
+//
+//   	@Override
+//   	public void tearDown() throws Exception {
+//        solo.finishOpenedActivities();
+//        super.tearDown();
+//  	}
+//
+//	public void testRun() {
+//        //Wait for activity: 'com.se491.chef_ly.activity.MainActivity'
+//		solo.waitForActivity(com.se491.chef_ly.activity.MainActivity.class, 2000);
+//        //Enter the text: 'a'
+//		solo.clearEditText((android.widget.EditText) solo.getView(com.se491.chef_ly.R.id.username));
+//		solo.enterText((android.widget.EditText) solo.getView(com.se491.chef_ly.R.id.username), "a");
+//        //Click on Empty Text View
+//		solo.clickOnView(solo.getView(com.se491.chef_ly.R.id.password));
+//        //Enter the text: 'a'
+//		solo.clearEditText((android.widget.EditText) solo.getView(com.se491.chef_ly.R.id.password));
+//		solo.enterText((android.widget.EditText) solo.getView(com.se491.chef_ly.R.id.password), "a");
+//        //Click on Sign In
+//		solo.clickOnView(solo.getView(com.se491.chef_ly.R.id.signInBtn));
+//        //Wait for activity: 'com.se491.chef_ly.activity.RecipeListActivity'
+//		assertTrue("com.se491.chef_ly.activity.RecipeListActivity is not found!", solo.waitForActivity(com.se491.chef_ly.activity.RecipeListActivity.class));
+//	}
+//}
