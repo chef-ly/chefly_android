6d5a52f13c220566c664be1e96a0cb5bb72a2efa
Wolf <brucknerw@gmail.com>
2017-04-08 16:53:25 -0500

    PocketSphinx stuff to getCooking

--- app/src/main/java/com/se491/chef_ly/activity/GetCookingActivity.java
+++ app/src/main/java/com/se491/chef_ly/activity/GetCookingActivity.java
@@ -1,13 +1,17 @@
 package com.se491.chef_ly.activity;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.speech.tts.TextToSpeech;
 import android.speech.RecognizerIntent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
 import android.view.View;
@@ -18,10 +22,21 @@ import android.widget.Toast;
 
 import com.se491.chef_ly.R;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
-public class GetCookingActivity extends Activity implements View.OnClickListener {
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
+
+import static android.widget.Toast.makeText;
+
+public class GetCookingActivity extends Activity implements View.OnClickListener, RecognitionListener {
 
     private Button prev;
     private Button next;
@@ -41,6 +56,23 @@ public class GetCookingActivity extends Activity implements View.OnClickListener
     private int white;
     private int step;
     private boolean hasDirections = false;
+
+    // PocketSphinx vars
+    /* Named searches allow to quickly reconfigure the decoder */
+    private static final String KWS_SEARCH = "wakeup";
+    private static final String MENU_SEARCH = "menu";
+    private static final String FORWARD = "next";
+    private static final String BACK = "back";
+    private static final String QUESTION_SEARCH = "question";
+
+    /* Keyword we are looking for to activate menu */
+    private static final String KEYPHRASE = "hey chef";
+
+    /* Used to handle permission request */
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+
+    private SpeechRecognizer recognizer;
+    private HashMap<String, Integer> captions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +109,31 @@ public class GetCookingActivity extends Activity implements View.OnClickListener
             }
         });
 
+        //Pocketsphinx vars and code
+        // Prepare the data for UI
+        captions = new HashMap<String, Integer>();
+        captions.put(KWS_SEARCH, R.string.kws_caption);
+        captions.put(MENU_SEARCH, R.string.menu_caption);
+        captions.put(FORWARD, R.string.forward_caption);
+        captions.put(BACK, R.string.back_caption);
+        captions.put(QUESTION_SEARCH, R.string.question_caption);
+
+        //((TextView) findViewById(R.id.text))
+        //        .setText("Preparing the recognizer");
+
+        Toast.makeText(this, "Preparing the recognizer", Toast.LENGTH_LONG).show();
+
+        // Check if user has given permission to record audio
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
+            return;
+        }
+
+
+
+        // -- end pocketsphinx
+        /*
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
@@ -94,7 +151,187 @@ public class GetCookingActivity extends Activity implements View.OnClickListener
                 }
             }
         });
+        */
+
+        //TODO - run recognizer after text is done being read
+        runRecognizerSetup();
     }
+
+    // --- PocketSphinx functions
+    private void runRecognizerSetup() {
+        // Recognizer initialization is a time-consuming and it involves IO,
+        // so we execute it in async task
+        new AsyncTask<Void, Void, Exception>() {
+            @Override
+            protected Exception doInBackground(Void... params) {
+                try {
+                    Assets assets = new Assets(GetCookingActivity.this);
+                    File assetDir = assets.syncAssets();
+                    setupRecognizer(assetDir);
+                } catch (IOException e) {
+                    return e;
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Exception result) {
+                if (result != null) {
+                    ((TextView) findViewById(R.id.text))
+                            .setText("Failed to init recognizer " + result);
+                } else {
+                    switchSearch(KWS_SEARCH);
+                }
+            }
+        }.execute();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                runRecognizerSetup();
+            } else {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (recognizer != null) {
+            recognizer.cancel();
+            recognizer.shutdown();
+        }
+    }
+
+    /**
+     * In partial result we get quick updates about current hypothesis. In
+     * keyword spotting mode we can react here, in other modes we need to wait
+     * for final result in onResult.
+     */
+    @Override
+    public void onPartialResult(Hypothesis hypothesis) {
+        if (hypothesis == null)
+            return;
+
+
+        // TODO - this is where the implementation happens
+        String text = hypothesis.getHypstr();
+        if (text.equals(KEYPHRASE))
+            switchSearch(MENU_SEARCH);
+        else if (text.equals(FORWARD)) {
+            //TODO - Put call to next button function here
+            next.performClick();
+            switchSearch(KWS_SEARCH);
+        }
+        else if (text.equals(BACK)) {
+            // TODO - put call to back button fuction
+            prev.performClick();
+            switchSearch(KWS_SEARCH);
+        }
+        else if (text.equals(QUESTION_SEARCH))
+            // TODO - implement getting text of the question and passing here
+            switchSearch(QUESTION_SEARCH);
+        else
+            //((TextView) findViewById(R.id.text)).setText(text);
+            Toast.makeText(this, "parital "+ text, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * This callback is called when we stop the recognizer.
+     */
+    @Override
+    public void onResult(Hypothesis hypothesis) {
+        //((TextView) findViewById(R.id.text)).setText("");
+        if (hypothesis != null) {
+            String text = hypothesis.getHypstr();
+            makeText(getApplicationContext(), "full " + text, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+    }
+
+    /**
+     * We stop recognizer here to get a final result
+     */
+    @Override
+    public void onEndOfSpeech() {
+        if (!recognizer.getSearchName().equals(KWS_SEARCH))
+            switchSearch(KWS_SEARCH);
+    }
+
+    private void switchSearch(String searchName) {
+        recognizer.stop();
+
+        // If we are not spotting, start listening with timeout (10000 ms or 10 seconds).
+        if (searchName.equals(KWS_SEARCH))
+            recognizer.startListening(searchName);
+        else
+            recognizer.startListening(searchName, 10000);
+
+        String caption = getResources().getString(captions.get(searchName));
+        //((TextView) findViewById(R.id.text)).setText(caption);
+        Toast.makeText(this, caption, Toast.LENGTH_LONG).show();
+    }
+
+    private void setupRecognizer(File assetsDir) throws IOException {
+        // The recognizer can be configured to perform multiple searches
+        // of different kind and switch between them
+
+        recognizer = SpeechRecognizerSetup.defaultSetup()
+                .setAcousticModel(new File(assetsDir, "en-us-ptm"))
+                .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
+
+                .setRawLogDir(assetsDir) // To disable logging of raw audio comment out this call (takes a lot of space on the device)
+
+                .getRecognizer();
+        recognizer.addListener(this);
+
+        /** In your application you might not need to add all those searches.
+         * They are added here for demonstration. You can leave just one.
+         */
+
+        // Create keyword-activation search.
+        recognizer.addKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
+
+        // Create grammar-based search for selection between demos
+        File menuGrammar = new File(assetsDir, "menu.gram");
+        recognizer.addGrammarSearch(MENU_SEARCH, menuGrammar);
+
+        // TODO - do we need these defined as grammar searches? NO?
+        // Create grammar-based search for digit recognition
+        //File digitsGrammar = new File(assetsDir, "digits.gram");
+        //recognizer.addGrammarSearch(FORWARD, digitsGrammar);
+
+        // Create grammar-based search for digit recognition
+        //File digits2Grammar = new File(assetsDir, "digits.gram");
+        //recognizer.addGrammarSearch(BACK, digits2Grammar);
+
+        // Create language model search
+        File languageModel = new File(assetsDir, "weather.dmp");
+        recognizer.addNgramSearch(QUESTION_SEARCH, languageModel);
+    }
+
+    @Override
+    public void onError(Exception error) {
+        ((TextView) findViewById(R.id.text)).setText(error.getMessage());
+    }
+
+    @Override
+    public void onTimeout() {
+        switchSearch(KWS_SEARCH);
+    }
+
+
+    // -- END POCKETSPHINX FUNCTIONS
 
     @Override
     protected void onStart() {
