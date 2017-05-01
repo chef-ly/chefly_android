a26c775b343787c6211e1b18e789efdcc3160f50
AthVerr <athina.verroiopoulou@gmail.com>
2017-05-01 00:04:39 -0500

    ingredients search

--- app/src/main/java/com/se491/chef_ly/activity/nav_activities/SearchIngredients.java
+++ app/src/main/java/com/se491/chef_ly/activity/nav_activities/SearchIngredients.java
@@ -13,7 +13,6 @@ import com.se491.chef_ly.R;
 public class SearchIngredients extends AppCompatActivity implements View.OnClickListener{
 
    private EditText ingredients;
-    private TextView email;
     private Button addIn;
     private Button findRecipeBtn;
     @Override
@@ -21,13 +20,8 @@ public class SearchIngredients extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_ingredients);
             ingredients = (EditText) findViewById(R.id.ingredient1EditText);
-
-            //findRecipe(selectedIngredient.toString());
-
-
             addIn = (Button) findViewById(R.id.addINBtn);
             addIn.setOnClickListener(this);
-
             findRecipeBtn = (Button) findViewById(R.id.findRecipes);
             findRecipeBtn.setOnClickListener(this);
     }
@@ -37,10 +31,9 @@ public class SearchIngredients extends AppCompatActivity implements View.OnClick
         switch (v.getId()) {
             case R.id.addINBtn:
                 Editable selectedIngredient = ingredients.getText();
+                //findRecipe(selectedIngredient.toString());
                 break;
             case R.id.findRecipes:
-
-
                 break;
 
         }
