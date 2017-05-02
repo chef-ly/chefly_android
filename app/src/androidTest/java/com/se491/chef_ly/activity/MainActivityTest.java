3910f5c293c8356c6012afc6932864a99e757e8d
AthVerr <athina.verroiopoulou@gmail.com>
2017-05-02 00:39:06 -0500

    fix warnings and add permissionGranter

---
+++ app/src/androidTest/java/com/se491/chef_ly/activity/MainActivityTest.java
@@ -0,0 +1,69 @@
+package com.se491.chef_ly.activity;
+
+//import timber.log.Timber;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.support.v4.content.ContextCompat;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.se491.chef_ly.PermissionGranter;
+import com.se491.chef_ly.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    private static final int PERMISSIONS_DIALOG_DELAY = 3000;
+    private static final int GRANT_BUTTON_INDEX = 1;
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+
+
+
+    @Test
+    public void mainActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+       // allowPermissionsIfNeeded();
+        PermissionGranter.allowPermissionsIfNeeded("RECORD_AUDIO");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.continueAsGuest), withText("Skip"),
+                        withParent(allOf(withId(R.id.activity_main),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+
+    }
+
+}
