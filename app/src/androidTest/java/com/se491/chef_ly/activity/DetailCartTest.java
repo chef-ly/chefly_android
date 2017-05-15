bb6398e6d11627063137a29e5ada3f4ccb6a3a42
AthVerr <athina.verroiopoulou@gmail.com>
2017-05-15 12:50:45 -0500

    done with testing

---
+++ app/src/androidTest/java/com/se491/chef_ly/activity/DetailCartTest.java
@@ -0,0 +1,138 @@
+package com.se491.chef_ly.activity;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.se491.chef_ly.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class DetailCartTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void detailCartTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.skipBtn), withText("Skip"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.continueAsGuest), withText("Skip"),
+                        withParent(allOf(withId(R.id.activity_main),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction relativeLayout = onView(
+                allOf(childAtPosition(
+                        withId(R.id.list),
+                        0),
+                        isDisplayed()));
+        relativeLayout.perform(click());
+        ViewInteraction textView = onView(
+                allOf(childAtPosition(
+                        withId(R.id.ingredientGroup),
+                        0),
+                        isDisplayed()));
+        textView.perform(scrollTo(), click());
+
+//        ViewInteraction textView = onView(
+//                withText("5 tablespoons almond milk (or milk)"));
+//        textView.perform(scrollTo(), click());
+
+        ViewInteraction appCompatImageButton = onView(
+                withId(R.id.directionsDropdown));
+        appCompatImageButton.perform(scrollTo(), click());
+
+        pressBack();
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction appCompatCheckedTextView = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Shopping List"), isDisplayed()));
+        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction linearLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.list),
+                                withParent(withId(R.id.activity_shopping_list))),
+                        0),
+                        isDisplayed()));
+        linearLayout.perform(click());
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.clearPurchasedBtn), withText("Clear Purchased"),
+                        withParent(allOf(withId(R.id.activity_shopping_list),
+                                withParent(withId(android.R.id.content)))),
+                        isDisplayed()));
+        button.perform(click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
